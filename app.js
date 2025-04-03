c46566415ef4bf6469b7e342499f1dc4a5e0bd93
Alaeddine-Elouaddah <166127907+Alaeddine-Elouaddah@users.noreply.github.com>
2025-04-03 10:54:50 +0000

    Correction des conflits

--- app.js
+++ app.js
@@ -1,36 +1,31 @@
-const http = require("http") ; 
-const books  = [
-    {
-        id : 1 , 
-        name : "Alae" 
-    } , {
-        id : 2 , 
-        name : "Akram " 
-    }
-]
-
-const server = http.createServer((req , res) => {
-   if(req.url === '/'){
-     res.write("<h1> Inchaalah Expert In Mern Stack   </h1> " ) ;   
-     res.end() ;      
-    } 
-    else if(req.url === '/api/books'){
-       res.write(JSON.stringify(books)) ; 
-       res.end() ; 
-    }
-    else if (req.url === '/api/books/:id'){
-        res.write("<h1>Id Not Found </h1>") ;
-        res.end() ;  
-    }
-    else if (req.url==='/api/books/ :name'){
-          res.write("<h2>Title Not Found </h2>") ;
-          res.end() ; 
-    }
-    else{
-         res.write("<h1>Page not Found   </h1>") ; 
-         res.end() ; 
-    }
+const express =  require("express") ; 
+const app  = express() ; 
+const PORT = 8000; 
+const books = [{
+        
+}] ; 
+app.get('/',(req,res)=>{
+     res.send("HELOO EXPRESS ! ") ; 
 }) ; 
- 
-const PORT = 5000; 
-server.listen(PORT, () => console.log(`Server is runnig on port ${PORT}`) ) ; 
+app.get('/api/books' , (req,res)=> {
+   res.send("WELCOME TO STORE API");
+}  ); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT , () => console.log(`Server is Running on Port ${PORT}`)) ; 
